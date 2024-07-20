b224f69b69f0391ddf37866a9a880d0f86637c70
Brian Flad <bflad417@gmail.com>
2024-07-20 15:28:19 -0400

    chore: Update URL pagination endpoint to always return 20 results
    Previously, it would always return 0 results, which was confusing. For testing purposes, it shouldn't matter that the results are repeated in this context.

--- internal/pagination/service.go
+++ internal/pagination/service.go
@@ -158,6 +158,11 @@ func HandleURL(w http.ResponseWriter, r *http.Request) {
 		ResultArray: make([]interface{}, 0),
 	}
 
+	// Just always return the same 20 results
+	for i := 0; i < total; i++ {
+		res.ResultArray = append(res.ResultArray, i)
+	}
+
 	if attempts > 1 {
 		baseURL := fmt.Sprintf("%s://%s", r.URL.Scheme, r.Host)
 		if r.URL.Scheme == "" { // Fallback if Scheme is not available
